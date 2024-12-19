963c5d00e04fbba258e1f36c5a79db33e17edffd
riwin <13813714+rmeza019@users.noreply.github.com>
2024-12-18 19:55:16 -0600

    add stats and behavior for drone enemy

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,12 +1,33 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+var speed = 300.0
+var active:bool = false
+var vulnerable:bool = true
+var health:int = 50
+
+func _ready() -> void:
+	$Explosion.hide()
 
 func _process(_delta: float) -> void:
-	var direction: Vector2 = Vector2.RIGHT
-	
-	velocity = direction * SPEED
-	move_and_slide()
+	if active:
+		var offset = PI / 2 
+		look_at(Globals.player_pos)
+		rotation += offset
+		var direction = (Globals.player_pos - position).normalized()
+		velocity = direction * speed
+		move_and_slide()
 
 func hit():
-	print("drone took damage")
+	if vulnerable:
+		vulnerable = false
+		$HitTimer.start()
+		health -= 10
+	
+	if health <= 0:
+		$AnimationPlayer.play("explosion")
+
+func _on_notice_area_body_entered(body: Node2D) -> void:
+	active = true
+
+func _on_hit_timer_timeout() -> void:
+	vulnerable = true
